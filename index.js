1dcd68ae809adb09d3654db6b8613039e70c756d
JackLCmore <145240145+JackLCmore@users.noreply.github.com>
2023-10-31 18:33:40 -0700

    add employee, role and department functionality now all working

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ const employeeQuestions = [
 
 const roleQuestions = [
     {
-        name:'name',
+        name:'title',
         type:'input',
-        message:'What is the name of the role?'
+        message:'What is the title of the role?'
     },
     {
         name:'salary',
@@ -52,13 +52,13 @@ const roleQuestions = [
         message:'Which department does the role belong to?',
         choices: ['Sales', 'Engineering', 'Finance', 'Legal']
     }
-]
+];
 
 const deptQuestions = [
     {
-        name:'dept',
+        name:'name',
         type:'input',
-        messages:'What is the name of the new department you would like to add?'
+        messages:'Add a new department by name'
     }
 ];
 
@@ -77,17 +77,15 @@ function openMenu(){
     .then((res)=>{
     determineChoice(res.menu)
     });
-}
+};
 
 function addEmployee(){
 
     inquirer
     .prompt(employeeQuestions)
     .then((res)=>{
-        console.log(res);
         db.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id)
-        VALUES ("${res.first_name}", "${res.last_name}", ${determineRoleID(res.role)}, ${determineManagerID(res.manager)})`,function (err,queryData){
-        console.log(queryData);
+        VALUES ("${res.first_name}", "${res.last_name}", ${determineRoleID(res.role)}, ${determineManagerID(res.manager)});`,function (err,queryData){
         db.query('SELECT * FROM employees', function (err, res){
             console.log("\n");
             console.table(res);
@@ -103,42 +101,32 @@ function addRole(){
     inquirer
     .prompt(roleQuestions)
     .then((res)=>{
-
-    if(res.dept === "Sales"){
-    db.query('',function (err, res){
-        console.log("\n")
-        console.table(res);
+        db.query(`INSERT INTO roles (title, salary, department_id)
+        VALUES ("${res.title}", ${parseInt(res.salary)}, ${determineDeptID(res.dept)});`,function (err,data){
+        db.query('SELECT * FROM roles', function (err, queryData){
+            console.log("\n");
+            console.table(queryData);
+        });
         return openMenu();
-    })}
-    else if(res.dept === "Engineering"){
-        db.query('',function (err, res){
-            console.log("\n")
-            console.table(res);
-            return openMenu();
-        })
-    }
-    else if(res.dept === "Finance"){
-        db.query('',function (err, res){
-            console.log("\n")
-            console.table(res);
-            return openMenu();
-        })
-    }
-    else if(res.dept === "Legal"){
-        db.query('',function (err, res){
-            console.log("\n")
-            console.table(res);
-            return openMenu();
-        })
-    }})
-   
+        });
+    });
 };
 
 function addDept(){
 
     inquirer
     .prompt(deptQuestions)
-    .then((res)=>console.log(res))
+    .then((res)=>{
+        console.log(res)
+        db.query(`INSERT INTO departments (name)
+        VALUES ("${res.name}")`,function (err,data){
+        db.query('SELECT * FROM departments', function (err, queryData){
+            console.log("\n");
+            console.table(queryData);
+        });
+        return openMenu();
+        });
+    });
    
 };
 
@@ -199,21 +187,35 @@ return 'null'
 }
 };
 
+function determineDeptID(input){
+if(input === 'Sales'){
+return 1;
+}
+else if (input === 'Engineering'){
+return 2;
+}
+else if (input === 'Finance'){
+return 3;
+}
+else if (input === 'Legal'){
+return 4;
+}
+}
+
 function determineChoice(choice){
 if (choice === 'View All Employees'){
 db.query('SELECT * FROM employees', function (err, res){
     console.log("\n");
     console.table(res);
 });
-console.log(choice);
 return openMenu();
 }
 else if(choice === 'Add Employee'){
-    console.log(choice);
+
     return addEmployee();
 }
 else if(choice === 'Update Employee Role'){
-    console.log(choice);
+
     return openMenu();
 }
 else if(choice === 'View All Roles'){
@@ -224,7 +226,7 @@ else if(choice === 'View All Roles'){
     return openMenu();
 }
 else if(choice === 'Add Role'){
-    console.log(choice);
+
     return addRole();
 }
 else if(choice === 'View All Departments'){
@@ -235,11 +237,11 @@ else if(choice === 'View All Departments'){
     return openMenu();
 }
 else if(choice === 'Add Department'){
-    console.log(choice);
+
     return addDept();
 }
 else if(choice === 'Close'){
-    console.log(choice);
+
     return;
 }
 };
